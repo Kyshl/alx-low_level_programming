e549287c237ef4af6550febc63471979b5d7b58a
Kyshl <keshlill94@gmail.com>
2023-03-21 07:06:39 -0700

    putchar updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,10 @@
 #include "main.h"
-	/**
-	 * main - print _putchar
-	 * 
-	 * Return: 0
-	 */
-
-
-	int main(void)
-	{
+/**
+ * main - print _putchar
+ * Return: 0
+ */
+int main(void)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -18,4 +15,4 @@
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-	}
+}
