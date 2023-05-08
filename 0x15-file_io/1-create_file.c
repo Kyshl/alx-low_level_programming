db25518b5968b1b965ac7608dad303f16916e608
Kyshl <keshlill94@gmail.com>
2023-05-08 08:28:02 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: content text to write to a file
+ *
+ * Return: 1 on success,-1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int te, k, l = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (l = 0; text_content[l];)
+			l++;
+	}
+
+	te = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	k = write(te, text_content, l);
+
+	if (te == -1 || k == -1)
+		return (-1);
+
+	close(te);
+
+	return (1);
+}
+
+
